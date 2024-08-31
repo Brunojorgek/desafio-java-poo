fa1bd5652a97f9a98c2dc453905c1d3ed4e5f2e3
Brunojorgek <brunojkassab@gmail.com>
2024-08-30 21:32:18 -0300

    Herança e Polimorfismo Parte1

--- src/desafio/Mentoria.java
+++ src/desafio/Mentoria.java
@@ -5,27 +5,29 @@ import java.time.LocalDate;
 
 import javax.xml.crypto.Data;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDate data;
 
-    
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
     }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public void setData(LocalDate data) {
         this.data = data;
     }
 
+    public LocalDate getData() {
+        return data;
+    }
 
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + data + "]";
+        return "Mentoria { " + 
+        "titulo = " + getTitulo() + '\'' + 
+        ", descricao = " + '\'' + getDescricao() + '\'' + 
+        ", data = " + data + "}"; 
     }
-
     
 }
